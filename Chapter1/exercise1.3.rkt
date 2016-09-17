38a2f9ae907d3562dc1bfacd13c95737a965bfe0
Typing <wzq9742@163.com>
2016-09-17 01:06:32 -0700

    add exercise solutions

--- Chapter1/exercise1.3.rkt
+++ Chapter1/exercise1.3.rkt
@@ -1,11 +1,11 @@
 ;; Exercise 1.3
-;; (define (p) (p))
-;; (define (test x y)
-;;   (if (= x 0)
-;;       0
-;;       y))
+(define (p) (p))
+(define (test x y)
+  (if (= x 0)
+      0
+      y))
 
-;; (test 0 (p))
+(test 0 (p))
 ;; In applicative-order evaluation, it will never terminate,
 ;; since (p) will evaluated forever.
 ;; In normal-order evaluation, (test 0 (p)) => (if (= 0 0) 0 (p)) => 0,
