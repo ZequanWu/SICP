72f3a0366c0dddaa5f6c32303d8009004e6bf469
Typing <wzq9742@163.com>
2016-09-17 23:35:00 -0700

    exercise 1.6

---
+++ Chapter1/exercise1.6.rkt
@@ -0,0 +1,18 @@
+;; Exercise 1.6
+#lang sicp
+(define (cube x) (* x x x))
+(define (improve x y) (/ (+ (/ x
+                               (* y y))
+                            (* 2 y))
+                         3))
+(define (good-enough? old-guess new-guess)
+  (< (abs (- old-guess new-guess)) 0.001))
+(define (cube-root-iter old-guess new-guess x)
+  (if (good-enough? old-guess new-guess)
+      new-guess
+      (cube-root-iter new-guess
+                      (improve x new-guess)
+                      x)))
+(define (cube-root x)
+  (cube-root-iter x 1 (* x 1.0)))
+                            
