88a4d334d8a57bad36c777e85f3bc797e44e5657
Typing <wzq9742@163.com>
2016-09-20 23:37:38 -0700

    remove ignored files

--- Chapter1/content.rkt
+++ Chapter1/content.rkt
@@ -29,3 +29,17 @@
         guess
         (sqrt-iter (improve guess))))
   (sqrt-iter 1))
+
+;; factorial function
+(define (factorial n)
+  (if (= n 1)
+      1
+      (* n (factorial (- n 1)))))
+
+(define (factorial2 n)
+  (define (iter product counter)
+    (if (> counter n)
+        product
+        (iter (* counter product)
+              (+ counter 1))))
+  (iter 1 1))
